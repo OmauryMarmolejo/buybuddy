1920cebea9dce6ad8f2b37f6fb169125c7c75134
Omaury Marmolejo <omaury@tango.io>
2023-08-01 22:17:02 -0600

    Add footer validation

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { Products } from "./components/Products";
 import { useState } from "react";
 import { Header } from "./components/Header";
 import { Footer } from "./components/Footer";
+import { IS_DEVELOPMENT } from "./config";
 
 function useFilters() {
   const [filters, setFilter] = useState({
@@ -32,7 +33,7 @@ function App() {
     <>
       <Header changeFilters={setFilter} />
       <Products products={filteredProducts} />
-      <Footer filters={filters} />
+      {IS_DEVELOPMENT && <Footer filters={filters} />}
     </>
   );
 }
